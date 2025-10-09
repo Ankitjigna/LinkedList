43f9d32c1137140321169afe7cbd995ae119f71e
Ankit Pal <114059115+Ankitjigna@users.noreply.github.com>
2025-10-09 21:38:59 +0530

    zig-zag means print first then last ,first++ and last--

---
+++ ZigZagLinkedList.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+public class ZigZagLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void zigZag() {
+        if (head == null || head.next == null) return; // empty or single node
+        // Find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // Reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Zig-zag merge
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        ZigZagLinkedList ll = new ZigZagLinkedList();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        ll.print(); // Original: 1 2 2 1
+        ll.zigZag(); // Call zig-zag transform
+        ll.print(); // Zig-zag: 1 1 2 2
+    }
+}
